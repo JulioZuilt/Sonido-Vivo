8ca510d73c557a29b37393112cef8bd49831675a
JulioZuilt <ju.zuilt@duocuc.cl>
2026-09-03 21:18:38 -0400

    Version 2.2: Creacion de los descuentos, y coherencia entre paginas

--- js/registro.js
+++ js/registro.js
@@ -103,6 +103,10 @@ formulario.addEventListener("submit", function(event) {
         return;
     }
 
+    if (fechaNacimiento.value >= "2014"){
+        errorFecha.textContent = "Ingresa un año valido."
+        return
+    }
 
     alert("Registro realizado correctamente.");
 
